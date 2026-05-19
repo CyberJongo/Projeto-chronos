9b8f3d1140310190e5d928ff7c1c63b558a95d4d
CyberJongo <joao.zuffo@murialdo.com.br>
2026-05-19 09:59:54 -0300

    Add Props + Children

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import { Heading } from "./components/Heading"
 
 
 export function App(){
+  console.log("Bye bye m")
   return(
     <Fragment>
-      <Heading></Heading>
+      <Heading>Hello World! 0</Heading>
+      <Heading>Hello World! 1</Heading>
+      <Heading>Hello World! 2</Heading>
       <h1>Eu amo lazanha</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
